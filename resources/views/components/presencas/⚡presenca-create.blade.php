84de799dc56b42d2503b6d926b9221f4b52b2ebf
Manuel Pedro <pedropedromanuel443@gmail.com>
2026-06-19 13:04:12 +0100

    fix: atualizar dependencias do composer para compatibilidade de PHP

--- resources/views/components/presencas/⚡presenca-create.blade.php
+++ resources/views/components/presencas/⚡presenca-create.blade.php
@@ -101,7 +101,7 @@ new class extends Component {
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none transition-colors cursor-pointer">
                 Confirmar Presença
             </button>
-            @canany
+            @endcanany
         </form>
     </section>
 
